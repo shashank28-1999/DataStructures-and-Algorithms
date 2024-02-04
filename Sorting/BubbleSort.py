87f3455a3c51cc3384d944114952a12b6d9e73b2
shashank <78431276+shashank28-1999@users.noreply.github.com>
2024-02-04 22:50:15 +0530

    Update BubbleSort.py

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,1 +1,15 @@
+def bubble_sort(arr):
+    for i in range(0,len(arr)):
+        for j in range(0,len(arr)):
+            if(arr[i]<arr[j]):
+                arr[i],arr[j]=arr[j],arr[i]
+    return arr
 
+# Taking input from the keyboard
+input_list = list(map(int, input("Enter space-separated numbers: ").split()))
+
+# Applying bubble sort
+sorted_list = bubble_sort(input_list)
+
+# Print the sorted output
+print("Sorted list:", sorted_list)
